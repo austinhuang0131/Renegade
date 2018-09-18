788738e48189ff3a0dbd25411e5badbdccc03c79
aTmG <43321147+aTmG@users.noreply.github.com>
2018-09-17 20:02:01 -0400

    no need for requiring Utils.js

--- events/messageCreateEvent.js
+++ events/messageCreateEvent.js
@@ -1,6 +1,5 @@
-const Utils = require("../Utils.js");
 module.exports = (bot) => {
     bot.on("messageCreate", async (msg) => {
-        await Utils.handleMessage(bot, msg);
+        await bot.utils.handleMessage(bot, msg);
     });
 }
