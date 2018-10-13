d8ee03eafc2d5b1e82fea3a0932981acfb3a2f87
aTmG <frostedcake884@gmail.com>
2018-10-13 15:31:19 -0400

    e s l i n t

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const Utils = require("./resources/Utils.js");
 const Eris = require("eris-additions")(require("eris"));
 const bot = new Eris(Utils.getSecrets().token, {
-  getAllUsers: true,
-  defaultImageFormat: "png",
-  defaultImageSize: 1024
+	getAllUsers: true,
+	defaultImageFormat: "png",
+	defaultImageSize: 1024
 });
 const { promisify } = require("util");
 const readdir = promisify(require("fs").readdir);
 const r = require("rethinkdbdash")({
-  db: "renegade_db"
+	db: "renegade_db"
 });
 
 Utils.db = r;
@@ -19,46 +19,45 @@ bot.secrets = bot.utils.getSecrets();
 bot.commands = [];
 
 const init = async () => {
+	async function loadCommands() {
+		const cmdContents = await readdir("./commands", { withFileTypes: true });
+		cmdContents.forEach(async content => {
+			if (content.isDirectory()) {
+				const category = content.name;
+				const cmdFiles = await readdir(`./commands/${category}`);
+				cmdFiles.forEach(f => {
+					if (!f.endsWith(".js")) return;
+					bot.commands[f.replace(/\..*/g, "")] = require(`./commands/${category}/${f}`);
+				});
+			} else {
+				if (!content.name.endsWith(".js")) return;
+				bot.commands[content.name.replace(/\..*/g, "")] = require(`./commands/${content.name}`);
+			}
+		});
+	}
 
-  async function loadCommands() {
-    const cmdContents = await readdir("./commands", { withFileTypes: true });
-    cmdContents.forEach(async content => {
-      if (content.isDirectory()) {
-        const category = content.name;
-        const cmdFiles = await readdir(`./commands/${category}`);
-        cmdFiles.forEach(f => {
-          if (!f.endsWith(".js")) return;
-          bot.commands[f.replace(/\..*/g, "")] = require(`./commands/${category}/${f}`);
-        });
-      } else {
-        if (!content.name.endsWith(".js")) return;
-        bot.commands[f.replace(/\..*/g, "")] = require(`./commands/${content.name}`);
-      }
-    });
-  }
+	async function loadEvents() {
+		const eventContents = await readdir("./events", { withFileTypes: true });
+		eventContents.forEach(async content => {
+			if (content.isDirectory()) {
+				const category = content.name;
+				const eventFiles = await readdir(`./events/${category}`);
+				eventFiles.forEach(f => {
+					if (!f.endsWith(".js")) return;
+					const event = require(`./events/${category}/${f}`);
+					event(bot);
+				});
+			} else {
+				if (!content.name.endsWith(".js")) return;
+				const event = require(`./events/${content.name}`);
+				event(bot);
+			}
+		});
+	}
 
-  async function loadEvents() {
-    const eventContents = await readdir("./events", { withFileTypes: true });
-    eventContents.forEach(async content => {
-      if (content.isDirectory()) {
-        const category = content.name;
-        const eventFiles = await readdir(`./events/${category}`);
-        eventFiles.forEach(f => {
-          if (!f.endsWith(".js")) return;
-          const event = require(`./events/${category}/${f}`);
-          event(bot);
-        });
-      } else {
-        if (!content.name.endsWith(".js")) return;
-        const event = require(`./events/${content.name}`);
-        event(bot);
-      }
-    });
-  }
-
-  await loadCommands();
-  await loadEvents();
-  await bot.connect();
-}
+	await loadCommands();
+	await loadEvents();
+	await bot.connect();
+};
 
 init();
